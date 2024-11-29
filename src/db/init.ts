59fa80778193aba18f79640d097375d6cdacf222
Saray Martinez <sm78544@lasallebajio.edu.mx>
2024-11-29 10:22:09 -0600

    fix sql

--- src/db/init.ts
+++ src/db/init.ts
@@ -21,5 +21,6 @@ export const dbConnection = async () => {
     } 
     catch (error) {
         console.log("Error connecting to database", error);
+        throw new Error("Error connecting to database");
     }
 };
